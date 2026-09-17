e662aaff2f2fda2286bf05865f031f0058ee0dc0
Carlos Jameux <carlos.jameux@inacapmail.cl>
2026-09-17 18:04:22 -0300

    Creación de la rama1 y app1 con sus respectivas vistas

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def vista1(request):
+    return HttpResponse("Hola desde la vista 1 de app1")
+def vista2(request):
+    return HttpResponse("Hola desde la vista 2 de app1")
